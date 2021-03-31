2958723c15634018031fe7ceb0c6b474fb8d7214
l4801015 <l4801015@gmail.com>
2021-03-31 12:55:16 +0300

    added complex plane

--- index.js
+++ index.js
@@ -16,6 +16,51 @@ const set = (n, c) => {
 		);
 }
 
-console.log(
-  set(10, complex(-0.2, -0.2))
+const re = (n, m, d) => delta(n * d, m * d, d);
+
+const im = (n, m, d) => delta(n * d, m * d, d);
+
+const delta = (n, m, d) => {
+	if(n == m) return m/d;
+	return [n/d].concat(delta(n + 1, m, d));
+};
+
+const imL = (imP, re) => {
+	if(re.length == 0) return [];
+	return [[imP, re[0]]].concat(imL(imP, re.slice(1)));
+};
+
+const reL = (reP, im) => {
+	if(im.length == 0) return [];
+	return [[reP, im[0]]].concat(reL(reP, im.slice(1)));
+};
+
+const pForIm = (im, re) => {
+	if(im.length == 0) return [];
+	return [imL(im[0], re)].concat(pForIm(im.slice(1), re));
+};
+
+const plane = (re, im) => {
+	if(re.length == 0) return [];
+	return [reL(re[0], im)].concat(
+		plane(re.slice(1), im)
+		);
+};
+
+const ms = plane => {
+	return set(1, complex(
+plane[0][0][0],
+plane[0][0][1]
 )
+);
+}
+
+console.log(
+ms(
+		plane(
+		  re(-3.1, 3.1, 10),
+		  im(-2.1, 2.1, 10)
+		)
+),
+		
+);
